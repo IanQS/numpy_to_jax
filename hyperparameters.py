d1eb2eb0dc3bed378a591ed219b503e9dfd6aff2
IQ <ian@ianq.ai>
2024-04-18 09:31:55 -0700

    save results for comparisons

--- hyperparameters.py
+++ hyperparameters.py
@@ -8,3 +8,5 @@ _tau_m = 20.0       # Membrane time constant (ms)
 _V_reset = -65.0    # Reset potential (mV)
 _V_thresh = -50.0   # Threshold potential (mV)
 _R = 1.0            # Membrane resistance (MΩ)
+
+num_simulations = 10
